18e96087082404daabb6d663f5ac2a96337e048f
dayofnemesis <guillaume.grin1995@gmail.com>
2014-08-28 19:40:09 -0400

    cul

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -91,9 +91,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(141, 82);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 26);
+            this.label1.Size = new System.Drawing.Size(96, 26);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Joueur 2k0";
+            this.label1.Text = "Joueur 1";
             // 
             // label2
             // 
